07fc9d3cb232217db3c8e374273462d34cc96aa3
admin <admin@192.168.0.5>
2018-09-27 11:46:56 +0530

    CRUD operation

---
+++ SB02-CRUD_TicketBookintMgmtApp/src/main/java/com/example/demo/Sb02CrudTicketBookintMgmtAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb02CrudTicketBookintMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb02CrudTicketBookintMgmtAppApplication.class, args);
+	}
+}
