bf234b8407548c4568314da2e83e99178cdf74a1
admin <admin@192.168.0.2>
2018-10-01 13:02:07 +0530

    Created project for Named native queries

---
+++ SB07-PeopleMgmtSpringDataNamedNativeQuery/src/main/java/com/mtp/app/Sb04PeopleMgmtSpringDataApplication.java
@@ -0,0 +1,39 @@
+package com.mtp.app;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.mtp.app.entities.Person;
+import com.mtp.app.service.PeopleMgmtService;
+
+@SpringBootApplication
+public class Sb04PeopleMgmtSpringDataApplication implements CommandLineRunner {
+
+	@Autowired
+	private PeopleMgmtService peopleMgmtService;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Sb04PeopleMgmtSpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		getPersonsInfoByFirstName();
+		//getPersonInfoByFirstNameAndEmail();
+	}
+
+	private void getPersonInfoByFirstNameAndEmail() {
+		List<Person> personList = peopleMgmtService.findByFirstNameAndEmail("Jitendra","jeet1.62.jp@gmail.com");
+		personList.forEach(System.out::println);
+	}
+
+	private void getPersonsInfoByFirstName() {
+		List<Person> personList = peopleMgmtService.getPersonInfoByFirstName("Jitendra");
+		personList.forEach(System.out::println);
+	}
+
+}
