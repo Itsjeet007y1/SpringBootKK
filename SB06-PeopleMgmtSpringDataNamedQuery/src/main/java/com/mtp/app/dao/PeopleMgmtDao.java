5dd56ae10e031b0abcf3d0cf73258864e0b3ab12
admin <admin@192.168.0.2>
2018-10-01 12:48:26 +0530

    Created project for named query

---
+++ SB06-PeopleMgmtSpringDataNamedQuery/src/main/java/com/mtp/app/dao/PeopleMgmtDao.java
@@ -0,0 +1,12 @@
+package com.mtp.app.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.mtp.app.entities.Person;
+
+public interface PeopleMgmtDao extends CrudRepository<Person, Integer> {
+	List<Person> getPersonInfoByFirstName(String lastName);
+	List<Person> findByFirstNameAndEmail(String firstName, String email);
+}
