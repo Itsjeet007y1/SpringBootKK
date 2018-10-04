68273b29a6a536c291bddc739ea9ae27c958d2ae
admin <admin@192.168.0.2>
2018-10-04 12:59:48 +0530

    Changing server port number various ways

---
+++ SB26-SpringBootChangingDefaultServerPort/src/main/java/com/mtp/app/Sb26SpringBootChangingDefaultServerPortApplication.java
@@ -0,0 +1,12 @@
+package com.mtp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb26SpringBootChangingDefaultServerPortApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb26SpringBootChangingDefaultServerPortApplication.class, args);
+	}
+}
