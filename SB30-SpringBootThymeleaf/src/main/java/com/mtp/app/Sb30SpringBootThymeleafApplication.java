154e7999afa079b11c9225bf756c151321dd950f
admin <admin@192.168.0.2>
2018-10-04 15:56:03 +0530

    Created project for spring boot thymeleaf

---
+++ SB30-SpringBootThymeleaf/src/main/java/com/mtp/app/Sb30SpringBootThymeleafApplication.java
@@ -0,0 +1,12 @@
+package com.mtp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb30SpringBootThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb30SpringBootThymeleafApplication.class, args);
+	}
+}
