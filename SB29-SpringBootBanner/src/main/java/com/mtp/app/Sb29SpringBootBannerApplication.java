ca52a4fc29edc8f5e7c2191229aca068d4f678a6
admin <admin@192.168.0.2>
2018-10-04 14:52:17 +0530

    Spring boot banner project

---
+++ SB29-SpringBootBanner/src/main/java/com/mtp/app/Sb29SpringBootBannerApplication.java
@@ -0,0 +1,12 @@
+package com.mtp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb29SpringBootBannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb29SpringBootBannerApplication.class, args);
+	}
+}
