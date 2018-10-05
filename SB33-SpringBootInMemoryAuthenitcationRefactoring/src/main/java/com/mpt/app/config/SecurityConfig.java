626e08c51b0cca2b201c285989892fc9445dc90d
admin <admin@192.168.0.2>
2018-10-05 10:52:44 +0530

    Refactored the previous project with spring boot security

---
+++ SB33-SpringBootInMemoryAuthenitcationRefactoring/src/main/java/com/mpt/app/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.mpt.app.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*http.csrf().disable().authorizeRequests().antMatchers("/api/tickets/ticket/**").
+			hasAnyRole("user","admin").and().formLogin();
+		http.csrf().disable().authorizeRequests().antMatchers("/api/tickets/admin/**").
+			hasAnyRole("admin").and().formLogin();*/
+		
+		// Refactoring of above code
+		
+		http.csrf().disable().authorizeRequests().antMatchers("/api/tickets/ticket/**").
+		hasAnyRole("user","admin").and().authorizeRequests().antMatchers("/api/tickets/admin/**").
+		hasAnyRole("admin").and().formLogin();
+	}
+	
+	@Autowired
+	public void configureGlobalBuilder(AuthenticationManagerBuilder auth) throws Exception {
+		/*auth.inMemoryAuthentication().withUser("Jeet").password("{noop}123").roles("user");
+		auth.inMemoryAuthentication().withUser("Jitu").password("{noop}1234").roles("user","admin");*/
+		
+		// Refactoring
+		auth.inMemoryAuthentication().withUser("Jeet").password("{noop}123").roles("user").and().
+		withUser("Jitu").password("{noop}1234").roles("user","admin");
+	}
+}
---
+++ SB33-SpringBootInMemoryAuthenitcationRefactoring/src/main/java/com/mpt/app/controller/TicketBookingController.java
@@ -0,0 +1,50 @@
+package com.mpt.app.controller;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mpt.app.entities.Ticket;
+import com.mpt.app.service.TicketBookingService;
+
+@RestController
+@RequestMapping(value = "/api/tickets")
+public class TicketBookingController {
+	
+	@Autowired
+	private TicketBookingService ticketBookingService;
+	
+	@PostMapping(value = "/create")
+	public Ticket createTicket(@RequestBody Ticket ticket) {
+		return ticketBookingService.createTicket(ticket);
+	}
+	
+	@GetMapping(value = "ticket/{ticketId}")
+	public Optional<Ticket> getTicketById(@PathVariable("ticketId") Integer ticketId) {
+		return ticketBookingService.TicketById(ticketId);
+	}
+	
+	@GetMapping(value = "admin/allTickets")
+	public Iterable<Ticket> getAllBookedTickets() {
+		return ticketBookingService.getAllTickets();
+	}
+	
+	@DeleteMapping(value = "ticket/{ticketId}")
+	public void deleteTicket(@PathVariable("ticketId") Integer ticketId) {
+		ticketBookingService.deleteTicket(ticketId);
+	}
+	
+	@PutMapping(value = "ticket/{ticketId}/{destination}")
+	public Ticket updateTicket(@PathVariable("ticketId") Integer ticketId, @PathVariable("destination") String destination) {
+		return ticketBookingService.updateTicket(ticketId, destination);
+	}
+}
